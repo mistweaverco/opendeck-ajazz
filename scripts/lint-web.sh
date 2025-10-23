6c6f450e0e5acf214d6935d6608e84eb174b3508
Marco Kellershoff <1384938+gorillamoe@users.noreply.github.com>
2025-10-23 12:42:57 +0200

    feat(web): add website + cleanup workflows (#5)
    * feat(web): add website + cleanup workflows
    
    * fix(ci): don't trigger on labelled

---
+++ scripts/lint-web.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd web || exit 1
+bun run lint
---
+++ web/src/routes/(root)/docs/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import HeadComponent from '$lib/HeadComponent.svelte';
+</script>
+
+<HeadComponent
+	data={{
+		title: 'opendeck-ajazz docs',
+		description: 'Documentation for the linux software for your Ajazz stream controller devices.'
+	}}
+/>
+
+<div id="start" class="hero bg-base-200 min-h-screen">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<img src="/logo.svg" alt="opendeck-ajazz logo" class="m-5 mx-auto w-64" />
+			<h1 class="text-5xl font-bold">opendeck-ajazz</h1>
+			<p class="py-6">
+				Documentation for the linux software for your Ajazz stream controller devices.
+			</p>
+			<p>Coming soon...</p>
+		</div>
+	</div>
+</div>
